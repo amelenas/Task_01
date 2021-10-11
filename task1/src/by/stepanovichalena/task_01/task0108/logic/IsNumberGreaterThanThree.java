073500116f5de97cd7416ceefd380416d712f6a5
amelenas <namealena@gmail.com>
2021-10-11 19:14:02 +0300

    Test_1

--- task1/src/by/stepanovichalena/task_01/task0108/logic/IsNumberGreaterThanThree.java
+++
@@ -1,7 +0,0 @@
-package by.stepanovichalena.task_01.task0108.logic;
-
-public class IsNumberGreaterThanThree {
-    public boolean isNumberGreaterThanTwo(double number) {
-        return number >= 3;
-    }
-}
